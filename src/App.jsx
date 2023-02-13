a53976a3ca7f978a73891f860bd9fe4517727116
kaderny <kaderbrklyn@outlook.com>
2023-02-13 12:50:20 -0600

    fixed dependency issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.scss";
-
+// import { useState, useEffect } from "react";
+// import ClipLoader from "react-spinners/ClipLoader";
 import Navbar from "./components/navbar/Navbar";
 import Landing from "./components/landing/Landing";
 import About from "./components/about/About";
@@ -11,8 +12,21 @@ import Inspiration from "./components/inspiration/Inspiration";
 import Footer from "./components/footer/footer";
 
 function App() {
+  //adding a loader here
+  // const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 8000);
+  // }, []);
+
   return (
     <div className="app">
+      {/* loading ?
+      <ClipLoader color={"#234BD1"} loading={loading} size={50} />
+      : */}
       <Navbar />
       <div className="sections">
         <Landing />
